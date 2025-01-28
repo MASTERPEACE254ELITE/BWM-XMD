e08078072d22b63e3f38c2cbd88a9c6a09e44513
MASTERPEACE254ELITE <aludabrian254@gmail.com>
2025-01-28 06:45:17 +0300

    Update bmwc.js

--- scs/bmwc.js
+++ scs/bmwc.js
@@ -1,6 +1,6 @@
 
 
-const { adams } = require("../Ibrahim/adams");
+const { adams } = require("../masterpeace/elite");
 const {getAllSudoNumbers,isSudoTableNotEmpty} = require("../lib/sudo")
 const conf = require("../config");
 
@@ -97,7 +97,7 @@ else {
 adams({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, auteurMessage, } = commandeOptions; 
  
-  repondre("THANK YOU FOR CHOOSING BMW, HERE ARE OUR SUPPORTIVE LINKS\n\n ☉ CHANNEL LINK IS HERE ☉ \n\n❒⁠⁠⁠⁠[https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y] \n\n ☉ GROUP LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://chat.whatsapp.com/F5BXJci8EDS9AJ6sfKMXIS] \n\n ☉YOUTUBE LINK IS HERE ☉\n\n❒⁠⁠⁠⁠[https://www.youtube.com/@ibrahimaitech] \n\n\n𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝒃𝒚 𝑰𝒃𝒓𝒂𝒉𝒊𝒎 𝑨𝒅𝒂𝒎𝒔") 
-  await zk.sendMessage(auteurMessage,{text : `THANK YOU FOR CHOOSING BMW-MD,MAKE SURE YOU FOLLOW THESE LINKS. `},{quoted :ms})
+  repondre("THANK YOU FOR CHOOSING masterpeace-techbot, contact masterpeace elite at 254743727510) 
+  await zk.sendMessage(auteurMessage,{text : `THANK YOU FOR CHOOSING mastertech-elite-MD, `},{quoted :ms})
 
 })
